35a014e345d7db3cd4b814fc6bf757c6d0b122ad
datny <robert.przydatek.cl@gmail.com>
2022-01-26 02:10:05 -0800

    ADD: Cron action

---
+++ pwc_addons/sale_ext/models/ext_models.py
@@ -0,0 +1,10 @@
+import logging
+
+from odoo import models, fields, api
+
+
+class SaleOlder(models.Model):
+    _inherit = ['sale.order.line']
+    conv_factor = fields.Integer(default=1)
+    customer_quantity = fields.Integer()
+
--- pwc_addons/sale_ext/models/models.py
+++ pwc_addons/sale_ext/models/models.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import datetime
 import logging
-from dateutil.relativedelta import relativedelta
 
 from odoo import models, fields, api
 
@@ -18,12 +17,25 @@ class SaleOlderArchive(models.Model):
     create_date = fields.Date(readonly=True, default=fields.Date.context_today)
 
     def _cron_archive_and_remove_selected(self):
-        """Creates new „sale.order.archive” object for all orders (sale.order) older than 7 days  and delete archived
-        „sale.order” object"""
+        """Creates new (sale.order.archive) object for orders (sale.order) older than 7 days  and delete (sale.order)
+        which have been archived"""
         _logger = logging.getLogger(__name__)
+        SaleOrder = self.env['sale.order']
+        SaleOlderArchive = self.env['sale.order.archive']
+        sale_orders_to_archive = []
         old_sale_orders = (
-            self.env["sale.order"]
+            SaleOrder
             .search([])
             .filtered(lambda p: (p.create_date.date() - datetime.date.today()).days > 7)
         )
-        _logger.info(msg=f"Archived and removed {old_sale_orders} orders.")
+        for order in old_sale_orders:
+            sale_orders_to_archive.append(dict(name=order.name,
+                                           customer=order.partner_id.id,
+                                           sale_person=order.user_id.id,
+                                           order_currency=order.currency_id.id,
+                                           order_total_amount=order.amount_total,
+                                           count_of_order_lines=len(order.order_line.ids),
+                                           create_date=order.create_date))
+            super(models.Model, order).unlink()
+        SaleOlderArchive.create(sale_orders_to_archive)
+        _logger.info(msg=f"Archived and removed {len(sale_orders_to_archive)} orders.")
